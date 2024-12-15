037690d4bf5e0dc4e457289e7a519fc86e314922
farukwira <81374711+farukwira@users.noreply.github.com>
2024-12-15 21:28:28 +0800

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,10 @@
+class Node {
+    String username, password;
+    Node next;
+
+    Node(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.next = null;
+    }
+}

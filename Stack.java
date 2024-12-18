5fb148bf44eaef1de54ec44fd2be5fd43a1dbd89
Zamzami Satria Tegar <gaarpotret@gmail.com>
2024-12-18 23:39:37 +0800

    Rekomendasi Makanan Stack

---
+++ Stack.java
@@ -0,0 +1,45 @@
+class Stack {
+    static class NodeStack {
+        String rekomendasi;
+        NodeStack next;
+
+        NodeStack(String rekomendasi) {
+            this.rekomendasi = rekomendasi;
+            this.next = null;
+        }
+    }
+
+    private NodeStack top;
+
+    void push(String rekomendasi) {
+        NodeStack baru = new NodeStack(rekomendasi);
+        if (top == null) {
+            top = baru;
+        } else {
+            baru.next = top;
+            top = baru;
+        }
+    }
+
+    String pop() {
+        if (top == null) {
+            return "Tidak ada riwayat.";
+        }
+        String rekomendasi = top.rekomendasi;
+        top = top.next;
+        return rekomendasi;
+    }
+
+    void display() {
+        if (top == null) {
+            System.out.println("Tidak ada riwayat rekomendasi.");
+            return;
+        }
+        System.out.println("Riwayat Rekomendasi Makanan:");
+        NodeStack temp = top;
+        while (temp != null) {
+            System.out.println("- " + temp.rekomendasi);
+            temp = temp.next;
+        }
+    }
+}

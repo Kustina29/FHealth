c8e5dc2e84ed3956d469ba881ddd7dd5d6fb45b8
kefza <baiqalfiaz@gmail.com>
2024-12-21 21:52:25 +0700

    Mainfix

--- Main.java
+++ Main.java
@@ -12,6 +12,10 @@ public class Main {
         daftarDokter.tambahDokter("Andre", "123");
         daftarDokter.tambahDokter("Bromo", "123");
         daftarDokter.tambahDokter("Chely", "123");
+        daftarDokter.tambahDokter("Sista", "123");
+        daftarDokter.tambahDokter("Hyuna", "123");
+        daftarDokter.tambahDokter("Fatur", "123");
+        daftarDokter.tambahDokter("Mutia", "123");
         while (true) {
             clearScreen();
             header();
@@ -68,10 +72,10 @@ public class Main {
 
     static void tampilkanMenuUtama() {
         System.out.println("\n+====================================================================+");
-        System.out.println("|                SELAMAT DATANG PADA APLIKASI FHEALTH                |");
+        System.out.println("|           Selamat Datang di FHealth, Solusi Sehat Anda!            |");
         System.out.println("+====================================================================+");
         System.out.println("|1. Sign In                                                          |");
-        System.out.println("|2. Log In                                                       |");
+        System.out.println("|2. Log In                                                           |");
         System.out.println("|3. Exit                                                             |");
         System.out.println("+====================================================================+");
         System.out.print("Pilih menu: ");
@@ -122,14 +126,15 @@ public class Main {
     static void menuUser(String username) {
         while (true) {
             clearScreen();
-            System.out.println("\n+==============================+");
-            System.out.println("|          MENU USER           |");
-            System.out.println("+==============================+");
+            System.out.println("\n+===============================================================+");
+            System.out.println("|                            MENU USER                          |");
+            System.out.println("+===============================================================+");
             System.out.println("1. Konsultasi Makanan");
             System.out.println("2. Cek Riwayat Rekomendasi");
             System.out.println("3. Cek Kategori Makanan");
             System.out.println("4. Cek Antrian");
-            System.out.println("5. Log Out");
+            System.out.println("5. Daftar Dokter");
+            System.out.println("6. Log Out");
             System.out.print("Pilih menu: ");
             int pilihan = input.nextInt();
             input.nextLine();
@@ -139,10 +144,12 @@ public class Main {
                     konsultasiMakanan(username);
                     break;
                 case 2:
+                    clearScreen();
                     riwayatRekomendasi.display();
                     input.nextLine();
                     break;
                 case 3:
+                    clearScreen();
                     kategoriMakanan.tampilkanKategori();
                     input.nextLine();
                     break;
@@ -152,6 +159,15 @@ public class Main {
                 input.nextLine();
                 break;
                 case 5:
+                clearScreen();
+                SortingDanSearch.bubbleSort(daftarDokter);
+                SortingDanSearch.printDokter(daftarDokter);
+                System.out.print("Masukkan nama dokter yang ingin dicari: ");
+                String namaDicari = input.nextLine(); 
+                SortingDanSearch.Search(daftarDokter, namaDicari);
+                input.nextLine();
+                break;
+                case 6:
                     return;
                 default:
                     System.out.println("Pilihan tidak valid. Silakan coba lagi.");
@@ -161,9 +177,9 @@ public class Main {
 
     static void konsultasiMakanan(String username) {
         clearScreen();
-        System.out.println("\n+==============================+");
-        System.out.println("|   JADWAL KONSULTASI DOKTER   |");
-        System.out.println("+==============================+");
+        System.out.println("\n+===============================================================+");
+        System.out.println("|                    JADWAL KONSULTASI DOKTER                   |");
+        System.out.println("+===============================================================+");
         System.out.println("Pilih Dokter untuk Konsultasi:");
         System.out.println("1. Dr. Andre (08:00 - 10:00)");
         System.out.println("2. Dr. Bromo (10:00 - 12:00)");
@@ -171,33 +187,38 @@ public class Main {
         System.out.print("Masukkan pilihan: ");
         int pilihan = input.nextInt();
         input.nextLine();
-
+    
         String dokter = "";
         switch (pilihan) {
             case 1:
-                dokter = "Dr. Andre";
+                dokter = "Andre";
                 break;
             case 2:
-                dokter = "Dr. Bromo";
+                dokter = "Bromo";
                 break;
             case 3:
-                dokter = "Dr. Chely";
+                dokter = "Chely";
                 break;
             default:
                 System.out.println("Pilihan tidak valid.");
                 return;
         }
-
-        antrianKonsultasi.enqueue(username, dokter);
+    
+        System.out.print("Masukkan keluhan Anda: ");
+        String keluhan = input.nextLine();
+    
+        antrianKonsultasi.enqueue(username, dokter, keluhan);
+        System.out.println("Anda telah berhasil mendaftar untuk konsultasi dengan Dr. " + dokter + ".");
         input.nextLine();
     }
+    
 
     static void konsultasiDanRekomendasi(String namaDokter) {
         clearScreen();
-        System.out.println("\n+==============================+");
-        System.out.println("|     KONSULTASI & REKOMENDASI |");
-        System.out.println("+==============================+");
-        System.out.print("Masukkan rekomendasi makanan untuk pasien: ");
+        System.out.println("\n+===============================================================+");
+        System.out.println("|                  KONSULTASI & REKOMENDASI                     |");
+        System.out.println("+===============================================================+");
+        System.out.print("Masukkan rekomendasi & saran untuk pasien: ");
         String rekomendasi = input.nextLine();
     
         riwayatRekomendasi.push("Dokter " + namaDokter + ": " + rekomendasi);
@@ -209,9 +230,9 @@ public class Main {
     static void menuDokter(String namaDokter) {
         while (true) {
             clearScreen();
-            System.out.println("\n+==============================+");
-            System.out.println("|         MENU DOKTER          |");
-            System.out.println("+==============================+");
+            System.out.println("\n+===============================================================+");
+            System.out.println("|                            MENU DOKTER                        |");
+            System.out.println("+===============================================================+");
             System.out.println("1. Ambil Antrian");
             System.out.println("2. Konsultasi ");
             System.out.println("3. Tambah Kategori Makanan");
@@ -228,6 +249,7 @@ public class Main {
                     konsultasiDanRekomendasi(namaDokter);
                     break;
                 case 3:
+                clearScreen();
                 tambahKategoriMakanan(kategoriMakanan, input);
                 case 4:
                     return;
@@ -239,51 +261,56 @@ public class Main {
 
 
     static void tambahKategoriMakanan(Tree kategoriMakanan, Scanner input) {
-        boolean lanjutMenambah = true; 
-        
+        boolean lanjutMenambah = true;
+        System.out.println("\n+===============================================================+");
+        System.out.println("|                       INPUT SARAN NUTRISI                     |");
+        System.out.println("+===============================================================+");
         while (lanjutMenambah) {
             System.out.print("Masukkan nama kategori baru: ");
-            String kategoriBaru = input.nextLine(); 
-            kategoriMakanan.tambahKategori(kategoriBaru); 
+            String kategoriBaru = input.nextLine();
+            kategoriMakanan.tambahKategori(kategoriBaru);
             System.out.println("Kategori baru berhasil ditambahkan.");
-        
-            System.out.print("Apakah Anda ingin menambahkan subkategori? (y/n): ");
-            String pilihan = input.nextLine(); 
-        
-            if (pilihan.equalsIgnoreCase("y||Y")) {
-                System.out.print("Masukkan kategori induk untuk subkategori: ");
-                String parentKategori = input.nextLine(); 
-        
-                System.out.print("Masukkan nama subkategori: ");
-                String subKategori = input.nextLine();
-        
-                kategoriMakanan.tambahSubKategori(parentKategori, subKategori); 
-        
-                System.out.println("Subkategori berhasil ditambahkan.");
-            } else {
-                System.out.println("Subkategori tidak ditambahkan.");
+    
+            System.out.print("Apakah Anda ingin menambahkan makanan ke kategori ini? (y/n): ");
+            String pilihan = input.nextLine();
+    
+            if (pilihan.equalsIgnoreCase("y")) {
+                boolean lanjutMakanan = true;
+                while (lanjutMakanan) {
+                    System.out.print("Masukkan nama makanan: ");
+                    String makanan = input.nextLine();
+                    kategoriMakanan.tambahMakananKeKategori(kategoriBaru, makanan);
+                    System.out.println("Makanan berhasil ditambahkan ke kategori " + kategoriBaru);
+    
+                    System.out.print("Apakah Anda ingin menambahkan makanan lagi ke kategori ini? (y/n): ");
+                    String tambahLagiMakanan = input.nextLine();
+                    if (tambahLagiMakanan.equalsIgnoreCase("n")) {
+                        lanjutMakanan = false;
+                        System.out.println("Proses penambahan makanan selesai.");
+                    }
+                }
+            } else if (pilihan.equalsIgnoreCase("n")) {
+                System.out.println("Tidak ada makanan yang ditambahkan ke kategori " + kategoriBaru);
             }
-            input.nextLine();
+    
             System.out.print("Apakah Anda ingin menambahkan kategori lagi? (y/n): ");
-            String tambahLagi = input.nextLine();
-            
-            if (!tambahLagi.equalsIgnoreCase("y || Y")) {
-                lanjutMenambah = false; 
+            String tambahLagiKategori = input.nextLine();
+    
+            if (!tambahLagiKategori.equalsIgnoreCase("y")) {
+                lanjutMenambah = false;
                 System.out.println("Proses penambahan kategori selesai.");
             }
         }
     }
     
+    
     static void ambilAntrianDokter(String namaDokter) {
-        System.out.println("\n+==============================+");
-        System.out.println("|         AMBIL ANTRIAN        |");
-        System.out.println("+==============================+");
         if (antrianKonsultasi.isEmpty()) {
             System.out.println("Tidak ada antrian untuk dokter " + namaDokter + ".");
         } else {
-            String pengguna = antrianKonsultasi.dequeue();
-            System.out.println("Pengguna " + pengguna + " sedang dilayani oleh dokter " + namaDokter + ".");
+            AntrianQueue.NodeQueue pasien = antrianKonsultasi.dequeue();
         }
         input.nextLine();
     }
+    
 }
